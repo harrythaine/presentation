f4624865134af02d59d6bd9a71423631b7511639
Harry <harrythaine@gmail.com>
2023-11-21 23:09:59 +0000

    trying tf with blank variables for access and secret keys

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "AWS_ACCESS_KEY_ID" {
-  default = var.AWS_ACCESS_KEY_ID
+  #default = var.AWS_ACCESS_KEY_ID
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
-  default = var.AWS_SECRET_ACCESS_KEY
+  #default = var.AWS_SECRET_ACCESS_KEY
 }
