5c995639a70b3207cc99897b3758c6ca8fd6336d
Harry <harrythaine@gmail.com>
2023-11-22 07:05:56 +0000

    cleaning up repo

--- main.tf
+++ main.tf
@@ -1,56 +1,9 @@
-# Requlon TF version to most recent
-/*
-terraform {
-  #requlond_version = "=1.6.4"
-   backend "s3" {
-    bucket         = "bucketname"
-    key            = "state.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "dydbname"
-    encrypt        = true
-    
-  }
-}
-*/
 # Download any stable version in AWS provider of 2.19.0 or higher in 2.19 train
 provider "aws" {
   region     = "eu-west-2"
   access_key = var.TF_VAR_AWS_ACCESS_KEY_ID  # This should reference your variable directly: var.AWS_ACCESS_KEY_ID
   secret_key = var.TF_VAR_AWS_SECRET_ACCESS_KEY  # Similar change here: var.AWS_SECRET_ACCESS_KEY
 }
-
-/*
-module "secureaccountterraform" {
-    source                                      = "./secureaccountterraform"
-    #provider
-    #IAM
-    Account_Alias                               = "presentation test account"
-    account_id                                  = ""
-    Config-Role-Name                            = "Config-Role"
-    #CloudTrail/ cloudwatch
-    CloudTrail-S3-Name                          = "htbcloudtrailloggingbucket"
-    CloudTrail-Trail                            = ""
-    cloudtrail_log_group_name                   = "CloudTrail/DefaultLogGroup"
-    alarm_namespace                             = ""
-    sns_topic_name                              = "" 
-    #VPC
-    VPC_Cidr                          = ""
-    VPC_Name                          = ""
-    VPC_Cidr_lon_pub_az1              = ""
-    VPC_Cidr_lon_pub_SN_az1_Name      = ""
-    VPC_Cidr_lon_pub_az2              = ""
-    VPC_Cidr_lon_pub_SN_az2_Name      = ""
-    VPC_Cidr_lon_priv_az1             = ""
-    VPC_Cidr_lon_priv_SN_az1_Name     = ""
-    VPC_Cidr_lon_priv_az2             = ""
-    VPC_Cidr_lon_priv_SN_az2_Name     = ""
-    #tagging
-    Environment                       = ""
-    Pillar                            = ""
-    Product                           = ""
-}
-*/
-
 
 module "lambda" {
   source              = "./chatbotterraform/lambda"
--- variables.tf
+++ variables.tf
@@ -1,3 +1,2 @@
 variable "TF_VAR_AWS_ACCESS_KEY_ID" {}
-
 variable "TF_VAR_AWS_SECRET_ACCESS_KEY" {}
